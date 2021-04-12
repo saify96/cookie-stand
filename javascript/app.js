ef65721799efc3ab7b5ba5585c3dc4bf9cb35085
saify96 <mohammad.alsaify@gmail.com>
2021-04-12 14:00:37 +0300

    m

--- javascript/app.js
+++ javascript/app.js
@@ -6,6 +6,9 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
+
+
+
 const seattle = {
   minCust:23,
   maxCust: 65,
